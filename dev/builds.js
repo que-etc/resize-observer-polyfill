e28620c87290c1b98cad48682f64944328c2484b
Denis Rul <que.etc@gmail.com>
2016-09-22 13:52:42 +0300

    Remove redundant babel plugins

--- dev/builds.js
+++ dev/builds.js
@@ -14,8 +14,10 @@ const builds = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015-loose', 'stage-1'],
-                    plugins: ['add-module-exports', 'transform-es2015-modules-commonjs']
+                    presets: [['es2015', {
+                        loose: true
+                    }]],
+                    plugins: ['add-module-exports']
                 }
             }]
         }
