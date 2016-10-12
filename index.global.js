030a6369419caa15fef7674451cf924fdc9da541
Denis Rul <que.etc@gmail.com>
2016-10-12 19:17:00 +0300

    Global ResizeObserver proprty shouldn't be enumerable

--- index.global.js
+++ index.global.js
@@ -1,6 +1,15 @@
-import ResizeObserver from './index';
+import ResizeObserver from './src/ResizeObserver';
 
-// Create a global reference to ResizeObserver.
-window.ResizeObserver = ResizeObserver;
+if (typeof window.ResizeObserver !== 'function') {
+    // ResizeObserver host property is not enumerable
+    // in the native implementation.
+    Object.defineProperty(window, 'ResizeObserver', {
+        value: ResizeObserver,
+        writable: true,
+        configurable: true
+    });
+}
 
-export default ResizeObserver;
+// Still export the constructor as for me it seems
+// awkward when a module doesn't export anything.
+export default window.ResizeObserver;
