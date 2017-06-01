a88b61561309df308f8c8d3eda9431f5d3fd70de
Denis Rul <que.etc@gmail.com>
2017-06-01 14:35:47 +0300

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -25,8 +25,10 @@ export default {
         }),
 
         uglify({
-            mangleProperties: {
-                regex: /_$/
+            mangle: {
+                properties: {
+                    regex: /_$/
+                }
             }
         })
     ]
