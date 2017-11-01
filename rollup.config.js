0ed57085f206e8af1b6da77ace5b0632322e0d08
Bogdan Chadkin <trysound@yandex.ru>
2017-11-01 13:04:45 +0300

    Upgrade build and untrack es dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import babel from 'rollup-plugin-babel';
 import buble from 'rollup-plugin-buble';
+const pkg = require('./package.json');
 
 export default {
-    entry: 'src/index.js',
-    targets: [{
-        moduleName: 'ResizeObserver',
-        dest: 'dist/ResizeObserver.js',
+    input: 'src/index.js',
+    output: [{
+        name: 'ResizeObserver',
+        file: pkg.main,
         format: 'umd'
     }, {
-        dest: 'dist/ResizeObserver.es.js',
+        file: pkg.module,
         format: 'es'
     }],
     plugins: [
