97e73eca0bfbad10fe904144a0a4af2ce3d75568
Denis Rul <que.etc@gmail.com>
2017-11-16 18:50:49 +0200

    Add special test environment for pull request (#26)

--- karma.conf.js
+++ karma.conf.js
@@ -197,6 +197,7 @@ module.exports = function (config) {
         ],
         plugins: [
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
             'karma-jasmine',
             'karma-jasmine-html-reporter',
             'karma-rollup-preprocessor',
