387220cd645e5bd1f43cc320185622d74d9db2a6
pyxarez <pyxarez@gmail.com>
2018-08-08 16:06:10 +0300

    Fix lint errors, rebuild package

--- src/utils/getWindowOf.js
+++ src/utils/getWindowOf.js
@@ -3,22 +3,22 @@ import global from '../shims/global.js';
 /**
  * Checks that target has a defaultView
  * @param  {Object} target
- * @return {Boolean}
+ * @returns {boolean}
  */
 function checkDefaultViewExisting(target) {
-  const simplyExists = target
-    && target.ownerDocument
-    && target.ownerDocument.defaultView;
+    const simplyExists = target &&
+    target.ownerDocument &&
+    target.ownerDocument.defaultView;
 
-  /**
+    /**
    * That case will work when target is an element inside of detached iframe.
    * In Chrome it will end up with null by "target.ownerDocument.defaultView" path
    * but in IE11 it will become a window with bunch of undefined properties so
    * we should follow the duck typing way.
    */
-  const nonEmptyWindow = !!(simplyExists && target.ownerDocument.defaultView.Object);
+    const nonEmptyWindow = !!(simplyExists && target.ownerDocument.defaultView.Object);
 
-  return nonEmptyWindow;
+    return nonEmptyWindow;
 }
 
 /**
@@ -31,9 +31,9 @@ export default target => {
     // Assume that the element is an instance of Node, which means that it
     // has the "ownerDocument" property from which we can retrieve a
     // corresponding global object.
-    var ownerGlobal = checkDefaultViewExisting(target)
-      ? target.ownerDocument.defaultView
-      : null;
+    const ownerGlobal = checkDefaultViewExisting(target) ?
+        target.ownerDocument.defaultView :
+        null;
 
     // Return the local global object if it's not possible extract one from
     // provided element.
